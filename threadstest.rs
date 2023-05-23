71e629dad350eb0fee9ae2feaa03b1fc3e6a8715
Wilson Gichuhi <55054375+Qodestackr@users.noreply.github.com>
2023-05-23 11:59:27 +0300

    Update threadstest.rs

--- threadstest.rs
+++ threadstest.rs
@@ -26,3 +26,9 @@ fn main() {
 //This is known as "capturing the environment" and allows closures
 //to access variables and perform actions within that captured context.
 
+//In the case of thread::spawn(move || { ... }), the closure is created and passed as 
+//an argument to the thread::spawn function. The closure captures the num variable from its 
+//surrounding environment and takes ownership of it using the move keyword. This allows the closure
+//to access and use the num variable within its own scope, 
+//even after the surrounding for loop has completed.
+
